eccbde0d7098e9268d783260361c62902a7f4458
William Goh <wirriamm@gmail.com>
2020-05-19 23:12:10 +0800

    g controller for citizens and added routes

---
+++ app/controllers/citizens_controller.rb
@@ -0,0 +1,31 @@
+class CitizensController < ApplicationController
+  def registration_form
+
+  end
+
+  def submit_registration
+
+  end
+
+  def signin_form
+
+  end
+
+  def submit_signin
+
+  end
+
+  def edit
+
+  end
+
+  def new
+
+  end
+
+  def create
+
+  end
+
+  private
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,13 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  get 'register', to: 'citizens#registration_form'
+  post 'citizens', to: 'citizens#submit_registration'
+
+  get 'signin', to: 'citizens#signin_form'
+  post 'signin', to: 'citizens#submit_signin'
+
+  put 'citizens/:id/edit', to: 'citizens#edit'
+
+  get 'citizens/:id/appointments/new', to: 'citizens#new'
+  post 'citizens/:id/appointments/create', to: 'citizens#create'
 end
