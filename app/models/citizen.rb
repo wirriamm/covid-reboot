36b05bd46a198ca2a405f1baf6bdbd890bb47fec
William Goh <wirriamm@gmail.com>
2020-05-19 21:43:32 +0800

    update citizens validation

--- app/models/citizen.rb
+++ app/models/citizen.rb
@@ -2,8 +2,8 @@ class Citizen < ApplicationRecord
   has_many :appointments
   has_many :clinics, through: :appointments
 
-  validates :email, presence: true, uniqueness: true
+  validates :email, presence: true, format: { with: /\A(|(([A-Za-z0-9]+_+)|([A-Za-z0-9]+\-+)|([A-Za-z0-9]+\.+)|([A-Za-z0-9]+\++))*[A-Za-z0-9]+@((\w+\-+)|(\w+\.))*\w{1,63}\.[a-zA-Z]{2,6})\z/i }, uniqueness: true
   validates :password, presence: true
   validates :postal_code, presence: true
-
+  validates :nric, format: { with: /\A[STFG]\d{7}[A-Z]\z/ }, presence: true, uniqueness: true
 end
